59d8f76a2ca795fd50a035eb5dfa49b238153fe0
andst <andst@itu.dk>
2021-02-08 12:40:50 +0100

    Started on the model

---
+++ itu-minitwit/homepage/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+from .models import User, Follower, Message, 
+
+
+class UserTests(TestCase):
+    def test_user(self):
+       pass 
+        
+class FollowerTests(TestCase):
+    def test_follower(self):
+       pass 
+        
+class MessageTests(TestCase):
+    def test_message(self):
+       pass 
+        
