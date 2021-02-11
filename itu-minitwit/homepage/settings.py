df8ea3cd4b426111e8765165040c6af644f41279
andst <andst@itu.dk>
2021-02-11 19:06:54 +0100

    added things needed for postgres

---
+++ itu-minitwit/homepage/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbtest', 
+        'USER': 'postgres', 
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
+    }
+}
