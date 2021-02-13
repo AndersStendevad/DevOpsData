bbfebec2f602dda8ce73423adafee77d40cf0504
Edi Begovic <edibegovic@gmail.com>
2021-02-14 00:42:30 +0100

    Missing slash/ in endpoint URL

--- minitwitAPI/minitwitAPI/base_api/urls.py
+++ minitwitAPI/minitwitAPI/base_api/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('msgs/<username>', views.UserMessagesView.as_view()),
-    path('msgs', views.MessagesView.as_view()),
+    path('msgs/', views.MessagesView.as_view()),
     path('fllws/<username>', views.UserFollowersView.as_view()),
     path('register/', views.RegistrationView.as_view()),
     path('latest/', views.LatestView.as_view()),
