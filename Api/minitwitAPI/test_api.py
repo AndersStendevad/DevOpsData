49ebe5334e23606e53ad9e1d18d1ed8b833ca8ec
Edi Begovic <edibegovic@gmail.com>
2021-02-26 18:20:16 +0100

    Added first test_latest()

--- Api/minitwitAPI/test_api.py
+++ Api/minitwitAPI/test_api.py
@@ -1,7 +1,21 @@
 import unittest
 from django.test import Client
+import base64
+import json
 
 class SimpleTest(unittest.TestCase):
+
+    BASE_URL = "http://localhost:8080"
+    USERNAME = "simulator"
+    PWD = "super_safe!"
+    CREDENTIALS = ":".join([USERNAME, PWD]).encode("ascii")
+    ENCODED_CREDENTIALS = base64.b64encode(CREDENTIALS).decode()
+    HEADERS = {
+        "Connection": "close",
+        "Content-Type": "application/json",
+        f"Authorization": f"Basic {ENCODED_CREDENTIALS}",
+    }
+
     def setUp(self):
         # Every test needs access to the request factory.
         self.client = Client()
@@ -10,3 +24,10 @@ class SimpleTest(unittest.TestCase):
         # Create an instance of a GET request.
         response = self.client.get('/msgs/')
         self.assertEqual(response.status_code, 403)
+
+    def test_latest(self):
+        url = "/register/?latest=1337"
+        data = {"username": "test", "email": "test@test", "pwd": "foo"}
+        response = self.client.post(url, data, content_type='application/json', HEADERS=self.HEADERS)
+        self.assertEqual(response.status_code, 204)
+       
