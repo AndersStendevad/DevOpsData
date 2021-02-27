b3ae8435d18e18e6f611d34226077302c0e8a936
Anna Reisz <annareisz@pop-os.localdomain>
2021-02-27 10:48:14 +0100

    added more tests to web

--- Web/itu_minitwit/minitwit/tests.py
+++ Web/itu_minitwit/minitwit/tests.py
@@ -1,17 +1,18 @@
 from django.test import TestCase
-
+from django.urls import reverse
 from .models import Profile, Follower, Message
-from .forms import SignInForm, SignUpForm
+from .forms import SignInForm, SignUpForm, PostForm
 
 
-class LogInViewTests(TestCase):
-    def test_sign_in(self):
-        pass
+
+
+def add_user():
+    p = Profile(username='karen', email='karen@karen.dk')
+    p.set_password("Manager2020")
+    p.save()
+    return p
 
 class MyTimelineViewTests(TestCase):
-    def test_post(self):
-        pass
-
     def test_usernames_me_and_followed(self):
         pass
 
@@ -21,12 +22,40 @@ class UserTimelineViewTests(TestCase):
 
 class PublicTimelineViewTests(TestCase):
     def test_public_timeline(self):
-        pass
+        response = self.client.get(reverse('public_timeline'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_no_messages(self):
+        response = self.client.get(reverse('public_timeline'))
+        self.assertContains(response, "There's no message so far.")
+
+    def test_messages(self):
+        p = add_user()
+        text = "I'd like to speak to the manager"
+        m = Message(author=p, content=text)
+        m.save()
+        response = self.client.get(reverse('public_timeline'))
+        self.assertEqual(len(response.context.get("messages")), 1)
+        self.assertEqual(response.context.get("messages")[0].get('text'), text)
+
+class PostFormTest(TestCase):
+    def test_post(self):
+        text = "I am really sorry, but the manager is not in today. I'm sure we can figure something out. How about the sorbet?"
+        form = PostForm(data={"content": text})
+        self.assertEquals(form.errors['content'], ["Ensure this value has at most 60 characters (it has 111)."])
 
 
 class SignInFormTests(TestCase):
-    def test_sign_in_form(self):
-        pass
+
+    def test_sign_in_form_fails(self):
+        response = self.client.post('/login/', {'username': 'bob', 'password': 'PornForAll'})
+        self.assertEqual(response.context['form'].errors['__all__'], ['Please enter a correct username and password. Note that both fields may be case-sensitive.'])
+
+    def test_sign_in_form_passes(self):
+        p = add_user()
+        response = self.client.post('/login/', {'username': 'karen', 'password': 'Manager2020'})
+        self.assertRedirects(response, '/timeline/', status_code=302, target_status_code=200, fetch_redirect_response=True)
+
 
 class SignUpFormTests(TestCase):
     def test_repeat_password(self):
@@ -53,25 +82,17 @@ class SignUpFormTests(TestCase):
 
 
 class ProfileTests(TestCase):
-    def add_user(self):
-        p = Profile(username='karen', email='karen@karen.dk')
-        p.set_password("Manager2020")
-        p.save()
-
     def test_add_user(self):
-        self.add_user()
+        add_user()
 
     def test_remove_user(self):
-        self.add_user()
-        p = Profile.objects.get(username='karen')
+        p = add_user()
         p.delete()
 
 
 class FollowerTests(TestCase):
     def make_users(self):
-        p = Profile(username='karen', email='karen@karen.dk')
-        p.set_password("Manager2020")
-        p.save()
+        p = add_user()
 
         p2 = Profile(username='kevin', email='kevin@kevin.dk')
         p2.set_password("password1234")
@@ -85,14 +106,7 @@ class FollowerTests(TestCase):
         f.save()
 
     def test_remove_follower(self):
-        p = Profile(username='karen', email='karen@karen.dk')
-        p.set_password("Manager2020")
-        p.save()
-
-        p2 = Profile(username='kevin', email='kevin@kevin.dk')
-        p2.set_password("password1234")
-        p2.save()
-
+        p, p2 = self.make_users()
         f = Follower(source_user=p, target_user=p2)
         f.save()
 
