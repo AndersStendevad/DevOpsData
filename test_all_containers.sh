afe8c95fa64dcbca366033b8b8be2954d15d5ab2
andst <andst@itu.dk>
2021-03-08 09:00:29 +0100

    Changed CI

--- test_all_containers.sh
+++ test_all_containers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./build_all_images.sh
-docker-compose down && docker-compose -f docker-compose.yaml up --remove-orphans -d  web_test api_test && docker-compose logs -f -t
+docker-compose down && docker-compose -f docker-compose.yaml up --abort-on-container-exit --remove-orphans -d  web_test api_test && docker-compose logs -f -t
