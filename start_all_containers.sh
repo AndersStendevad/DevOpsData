e5c53f11f6b5bdc0611df60040eb6a8bef0be05e
andst <andst@itu.dk>
2021-03-08 08:41:26 +0100

    Made changes for testing

--- start_all_containers.sh
+++ start_all_containers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./build_all_images.sh
-docker-compose -f docker-compose.yaml up --remove-orphans
+docker-compose down && docker-compose -f docker-compose.yaml up --remove-orphans -d web api && docker-compose logs -f -t
