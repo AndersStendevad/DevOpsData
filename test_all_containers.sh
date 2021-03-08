91d58c3836da33d1f8ac241fb07000278c225874
andst <andst@itu.dk>
2021-03-08 10:47:33 +0100

    Added test to web

--- test_all_containers.sh
+++ test_all_containers.sh
@@ -4,4 +4,9 @@ docker-compose up \
 --abort-on-container-exit \
 --exit-code-from api_test && \
 docker-compose down
+docker-compose up \
+--abort-on-container-exit \
+--exit-code-from web_test && \
+docker-compose down
+
 
