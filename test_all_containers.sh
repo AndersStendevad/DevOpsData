f16892cefe6673ff39c96b19f5c57956583c7dd4
Anna Reisz <annareisz@pop-os.localdomain>
2021-03-10 19:32:29 +0100

    fixing tests in progress

--- test_all_containers.sh
+++ test_all_containers.sh
@@ -2,13 +2,11 @@
 docker-compose build \
 && \
 docker-compose up \
---abort-on-container-exit \
 --exit-code-from api_test \
 && \
 docker-compose down \
 && \
 docker-compose up \
---abort-on-container-exit \
 --exit-code-from web_test \
 && \
 docker-compose down
