f4309c5678df855041b3045dffdbfc6d01caa6e5
Anna Reisz <annareisz@pop-os.localdomain>
2021-03-14 15:59:29 +0100

    added new allowed host

--- Api/minitwitAPI/global/settings.py
+++ Api/minitwitAPI/global/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "rbhd61f#1$f$4zjq_^ca0ll0f^qef(=0um5qgz*#w6f8abymjn"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["157.230.77.141", "139.59.142.18", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["157.230.77.141", "139.59.142.18", "localhost", "127.0.0.1", "134.209.248.68"]
 
 
 # Application definition
