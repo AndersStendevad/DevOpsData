79c7e521ae456f6cc5207835b60db1359fc7e1b8
andst <andst@itu.dk>
2021-03-14 18:07:17 +0100

    added push to test server

--- run_from_docker_hub.sh
+++ run_from_docker_hub.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull && \
 docker-compose -f deploy-compose.yaml down --remove-orphans && \
 docker rmi $(docker images -a -q)
 docker-compose -f deploy-compose.yaml up -d --force-recreate
