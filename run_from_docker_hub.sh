67d254cf5cefa73bcf5ab656a026bc187a479b59
Anders Stendevad <andersstendevad98@gmail.com>
2021-03-14 17:07:57 +0100

    Update run_from_docker_hub.sh

--- run_from_docker_hub.sh
+++ run_from_docker_hub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker-compose -f deploy-compose.yaml down && \
-docker rmi $(docker images -a -q) && \
+docker-compose -f deploy-compose.yaml down --remove-orphans && \
+docker rmi $(docker images -a -q)
 docker-compose -f deploy-compose.yaml up -d --force-recreate
