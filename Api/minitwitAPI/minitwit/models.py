26cec218f21a8f94c517c7dc16b4be0435c315b3
Anna Reisz <annareisz@pop-os.localdomain>
2021-03-21 13:35:40 +0100

    added user monitoring

--- Api/minitwitAPI/minitwit/models.py
+++ Api/minitwitAPI/minitwit/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django_prometheus.models import ExportModelOperationsMixin
 
-class Profile(AbstractUser):
+class Profile(ExportModelOperationsMixin("user")AbstractUser):
     class Meta:
         db_table = "profiles"
 
